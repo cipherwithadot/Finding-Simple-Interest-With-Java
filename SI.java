b4fadb43ce731e404bb4c5b0af56a06752972bc6
Cipher <88819188+Cipher-GF@users.noreply.github.com>
2022-06-07 12:53:11 +0530

    Create SI.java

---
+++ SI.java
@@ -0,0 +1,20 @@
+public class SI{
+	
+    public static void main (String args[]){
+    
+    int P = 1000;
+    int R = 10;
+    int T = 5;
+    
+    System.out.println("");
+    
+    System.out.println("The Principal Is: " + P + "$");
+    System.out.println("The Rate Is:" + R + "%");
+    System.out.println("T = " + T);
+    
+    System.out.println("");
+    
+    System.out.println("The Simple Interest: " + (P * R * T / 100) + "$");
+    
+    }
+}
